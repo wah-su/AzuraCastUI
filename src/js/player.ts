4d83064a0d162b413093c7696c7b44db4e9a5960
Radiquum <kentai.waah@gmail.com>
2025-10-18 21:52:50 +0500

    rewrite code to separate files

---
+++ src/js/player.ts
@@ -0,0 +1,340 @@
+import { tryCatch } from "./tryCatch";
+import {
+  computeElapsedTime,
+  extractCoverColorsAndSetBackgroundColors,
+  getActiveStation,
+  getAllElementByQuery,
+  getElementById,
+  getElementByQuery,
+  getVolume,
+  removeAllChildNodes,
+  secondsToMinutes,
+  setActiveStation,
+  updateTimestamps,
+} from "./utils";
+import Hls from "hls.js";
+
+const BASE_URL = process.env.BUN_PUBLIC_BASE_URL;
+let hls: Hls | null = null;
+let updatePlayTimerInterval: any = null;
+
+export function updatePlayTimer(playedAt: number, duration: number) {
+  const playerTimerStart =
+    getElementById<HTMLParagraphElement>("playerTimerStart");
+  const playerTimerTotal =
+    getElementById<HTMLParagraphElement>("playerTimerTotal");
+  const playerProgress = getElementById<HTMLDivElement>("playerProgress");
+  if (playerTimerStart)
+    playerTimerStart.textContent = secondsToMinutes(
+      computeElapsedTime(playedAt)
+    );
+  if (playerTimerTotal)
+    playerTimerTotal.textContent = secondsToMinutes(duration);
+  if (playerProgress)
+    playerProgress.style = `--played-percent:${
+      (computeElapsedTime(playedAt) / duration) * 100
+    }%;`;
+}
+
+export function createSongElement(played_at: number, song: any) {
+  const songTemplate = getElementById<HTMLTemplateElement>("SongTemplate");
+  if (!songTemplate) {
+    console.error("No song template found!");
+    return;
+  }
+
+  const nextSongTemplate = songTemplate.content.cloneNode(
+    true
+  ) as HTMLDivElement;
+  if (nextSongTemplate.children[0]) {
+    if (nextSongTemplate.children[0].children[0]) {
+      const img = nextSongTemplate.children[0].children[0] as HTMLImageElement;
+      img.src = song.art;
+    }
+
+    if (
+      nextSongTemplate.children[0].children[1] &&
+      nextSongTemplate.children[0].children[1].children[0] &&
+      nextSongTemplate.children[0].children[1].children[1] &&
+      nextSongTemplate.children[0].children[1].children[2]
+    ) {
+      const textContainer = nextSongTemplate.children[0]
+        .children[1] as HTMLDivElement;
+      const playedAt = textContainer.children[0] as HTMLParagraphElement;
+      const title = textContainer.children[1] as HTMLParagraphElement;
+      const artist = textContainer.children[2] as HTMLParagraphElement;
+      playedAt.dataset.playedTimestamp = String(played_at);
+      title.textContent = song.title;
+      artist.textContent = song.artist;
+    }
+  }
+  return nextSongTemplate;
+}
+
+export async function fetchStationNowPlaying(id: number) {
+  const { data: stationRequest } = await tryCatch(
+    fetch(`${BASE_URL}/nowplaying/${id}`)
+  );
+  if (!stationRequest) {
+    console.error(`Failed to fetch now playing for station ${id}`);
+    return false;
+  }
+  const data = await stationRequest.json();
+
+  const playerCover = getElementById<HTMLImageElement>("playerCover");
+  const playerTitle = getElementById<HTMLParagraphElement>("playerTitle");
+  const playerArtist = getElementById<HTMLParagraphElement>("playerArtist");
+
+  if (playerCover) playerCover.src = data.now_playing.song.art;
+  extractCoverColorsAndSetBackgroundColors();
+  if (playerTitle) playerTitle.textContent = data.now_playing.song.title;
+  if (playerArtist) playerArtist.textContent = data.now_playing.song.artist;
+  updatePlayTimer(data.now_playing.played_at, data.now_playing.duration);
+
+  const services = getAllElementByQuery<HTMLAnchorElement>(
+    "[data-service-prefix]"
+  );
+  if (!services) {
+    console.error("No services found!");
+  } else {
+    services.forEach((element) => {
+      element.href = `${element.dataset.servicePrefix}${data.now_playing.song.artist} - ${data.now_playing.song.title}`;
+    });
+  }
+
+  // @ts-ignore
+  if (updatePlayTimerInterval) {
+    clearInterval(updatePlayTimerInterval);
+  }
+
+  updatePlayTimerInterval = setInterval(() => {
+    if (
+      data.now_playing.duration -
+        computeElapsedTime(data.now_playing.played_at) <=
+      0
+    ) {
+      // @ts-ignore
+      clearInterval(this);
+      fetchStationNowPlaying(id);
+      return;
+    }
+
+    updatePlayTimer(data.now_playing.played_at, data.now_playing.duration);
+  }, 1000);
+
+  const pageTitle = getElementByQuery<HTMLTitleElement>("title");
+  if (pageTitle) {
+    pageTitle.textContent = `${data.now_playing.song.artist} - ${data.now_playing.song.title} | ${data.station.name}`;
+  }
+
+  const nextSongContainer = getElementById<HTMLDivElement>("nextSongContainer");
+  if (nextSongContainer) {
+    removeAllChildNodes(nextSongContainer);
+    const elem = createSongElement(
+      data.playing_next.played_at,
+      data.playing_next.song
+    );
+    if (elem) {
+      nextSongContainer.appendChild(elem);
+    }
+  }
+
+  const historySongContainer = getElementById<HTMLDivElement>(
+    "historySongContainer"
+  );
+  if (historySongContainer) {
+    removeAllChildNodes(historySongContainer);
+    data.song_history.forEach((song: any, index: number) => {
+      if (index > 3) return;
+      const elem = createSongElement(song.played_at, song.song);
+      if (elem) {
+        historySongContainer.appendChild(elem);
+      }
+    });
+  }
+
+  updateTimestamps();
+}
+
+function stopMedia() {
+  const audioElement = getElementByQuery<HTMLAudioElement>("audio");
+  if (audioElement) {
+    audioElement.pause();
+    if (hls) {
+      hls.detachMedia();
+      hls.destroy();
+    }
+    audioElement.src = "";
+    audioElement.remove();
+  }
+}
+
+export function setStation(next: number) {
+  const newLink = getElementByQuery<HTMLButtonElement>(
+    `button[data-station-id="${next}"]`
+  );
+  const allLinks = getAllElementByQuery<HTMLButtonElement>("[data-station-id]");
+  if (allLinks) {
+    allLinks.forEach((element) => {
+      element.style = "--link-opacity:75%;";
+      element.dataset.active = "false";
+    });
+  }
+  if (newLink) {
+    newLink.style = "--link-opacity:100%;";
+    newLink.dataset.active = "true";
+  }
+
+  const playButton = getElementById<HTMLButtonElement>("playButton");
+  if (!playButton || !playButton.children[0] || !playButton.children[1]) {
+    console.error("No play button found!");
+    return;
+  }
+  if (playButton.dataset.active == "true") {
+    playButton.children[0].classList.toggle("hidden");
+    playButton.children[1].classList.toggle("hidden");
+    playButton.dataset.active = "false";
+  }
+
+  stopMedia();
+  setActiveStation(next);
+  fetchStationNowPlaying(next);
+}
+
+export async function getAndPopulateStations() {
+  const stationSelectContainer = getElementById<HTMLDivElement>(
+    "stationSelectContainer"
+  );
+  if (!stationSelectContainer) {
+    console.error("No station select container found!");
+    return false;
+  }
+
+  const { data: stationRequest } = await tryCatch(
+    fetch(`${BASE_URL}/stations`)
+  );
+  if (!stationRequest) {
+    console.error("Failed to fetch station list");
+    return false;
+  }
+
+  const data = await stationRequest.json();
+
+  const stationTemplate = getElementById<HTMLTemplateElement>(
+    "StationLinkTemplate"
+  );
+  if (!stationTemplate) {
+    console.error("No station template found!");
+    return false;
+  }
+
+  data.forEach((station: any) => {
+    const templ = stationTemplate.content.cloneNode(
+      true
+    ) as HTMLTemplateElement;
+    if (!templ.children[0]) {
+      console.error("Failed to clone station select button template!");
+      return false;
+    }
+    const btn = templ.children[0] as HTMLButtonElement;
+    btn.textContent = station.name;
+    btn.dataset.stationId = station.id;
+    btn.dataset.stationShortcode = station.shortcode;
+    btn.dataset.stationName = station.name;
+    btn.setAttribute("id", `station_${station.id}`);
+    stationSelectContainer.appendChild(btn);
+  });
+
+  setStation(getActiveStation());
+
+  const stations = getAllElementByQuery<HTMLButtonElement>("[data-station-id]");
+  if (!stations) {
+    console.error("No stations found!");
+    return false;
+  }
+
+  stations.forEach((station) => {
+    if (!station.dataset.stationId) {
+      console.error("No station id found for button:", station);
+      return;
+    }
+    const { stationId } = station.dataset;
+    station.addEventListener("click", () => {
+      setStation(parseInt(stationId));
+    });
+  });
+}
+
+function togglePlayIcon(
+  playButton: HTMLButtonElement,
+  state: "play" | "pause"
+) {
+  if (!playButton || !playButton.children[0] || !playButton.children[1]) {
+    console.error("No play button found!");
+    return;
+  }
+
+  const PauseIcon = playButton.children[0];
+  const PlayIcon = playButton.children[1];
+
+  switch (state) {
+    case "play":
+      PlayIcon.classList.remove("hidden");
+      PauseIcon.classList.add("hidden");
+      break;
+    case "pause":
+      PlayIcon.classList.add("hidden");
+      PauseIcon.classList.remove("hidden");
+      break;
+  }
+}
+
+export async function toggleRadio() {
+  const activeStationId = getElementByQuery<HTMLButtonElement>(
+    "button[data-active='true']"
+  );
+
+  if (!activeStationId) {
+    console.error("No active station found!");
+    return false;
+  }
+
+  const { data: stationRequest } = await tryCatch(
+    fetch(`${BASE_URL}/nowplaying/${activeStationId.dataset.stationId}`)
+  );
+  if (!stationRequest) {
+    console.error(
+      `Failed to fetch now playing for station ${activeStationId.dataset.stationId}`
+    );
+    return false;
+  }
+  const data = await stationRequest.json();
+
+  const playButton = getElementById<HTMLButtonElement>("playButton");
+  if (!playButton) {
+    console.error("No play button found!");
+    return false;
+  }
+
+  if (playButton.dataset.active == "true") {
+    togglePlayIcon(playButton, "pause");
+    playButton.dataset.active = "false";
+    stopMedia();
+  } else {
+    togglePlayIcon(playButton, "play");
+    playButton.dataset.active = "true";
+
+    const el = document.createElement("audio");
+    const sr = data.station.hls_url;
+
+    const bodyEl = getElementByQuery<HTMLBodyElement>("body");
+    if (bodyEl) {
+      bodyEl.appendChild(el);
+      hls = new Hls();
+      hls.loadSource(sr);
+      hls.attachMedia(el);
+      el.volume = getVolume() / 100;
+      el.play();
+    }
+  }
+}
---
+++ src/js/utils.ts
@@ -0,0 +1,241 @@
+import ColorThief from "colorthief";
+
+export function getElementById<T>(id: string): T | null {
+  return (document.getElementById(id) as T) || null;
+}
+
+export function getElementByQuery<T>(query: string): T | null {
+  return (document.querySelector(query) as T) || null;
+}
+
+export function getAllElementByQuery<T>(query: string): T[] | null {
+  const elements = Array.from(document.querySelectorAll(query)) as T[];
+  if (elements.length == 0) {
+    return null;
+  }
+  return elements;
+}
+
+export function setActiveStation(id: number): void {
+  localStorage.setItem("station_id", String(id));
+}
+
+export function getActiveStation(): number {
+  const stationId = localStorage.getItem("station_id")
+    ? Number(localStorage.getItem("station_id"))
+    : null;
+  if (!stationId) {
+    setActiveStation(1);
+    return 1;
+  }
+  return stationId;
+}
+
+function changeVolumeIcon(volume: number): void {
+  const elements = getAllElementByQuery<HTMLImageElement>(
+    "[data-max-show-volume]"
+  );
+  if (!elements) {
+    console.error("No volume icons found!");
+    return;
+  }
+
+  for (const element of elements) {
+    if (!element.dataset.maxShowVolume) {
+      console.error("No max show volume found for element:", element);
+      continue;
+    }
+    if (!element.dataset.minShowVolume) {
+      console.error("No min show volume found for element:", element);
+      continue;
+    }
+    if (
+      volume > Number(element.dataset.minShowVolume) &&
+      volume <= Number(element.dataset.maxShowVolume)
+    ) {
+      element.classList.remove("hidden");
+    } else {
+      element.classList.add("hidden");
+    }
+  }
+}
+
+export function getVolume(): number {
+  return localStorage.getItem("volume")
+    ? Number(localStorage.getItem("volume"))
+    : 50;
+}
+
+export function setVolume(): void {
+  const audioElement = getElementByQuery<HTMLAudioElement>("audio");
+  const volumeSlider = getElementById<HTMLInputElement>("volumeSlider");
+
+  if (!volumeSlider) {
+    console.error("No volume slider found!");
+    return;
+  }
+
+  if (audioElement) {
+    audioElement.volume = Number(volumeSlider.value) / 100;
+  }
+
+  changeVolumeIcon(Number(volumeSlider.value));
+  localStorage.setItem("volume", String(volumeSlider.value));
+}
+
+export function removeAllChildNodes(parent: HTMLElement): void {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+export function secondsToMinutes(
+  seconds: number,
+  minutesPad: number = 0,
+  showMinutes: boolean = true,
+  showSeconds: boolean = true
+): string {
+  const mins = Math.floor(seconds / 60);
+  const secs = seconds - mins * 60;
+  return `${showMinutes ? String(mins).padStart(minutesPad, "0") : ""}${
+    showMinutes && showSeconds ? ":" : ""
+  }${showSeconds ? String(secs).padStart(2, "0") : ""}`;
+}
+
+export function computeElapsedTime(playedAtTimestamp: number): number {
+  const playedAt = new Date(playedAtTimestamp * 1000);
+  const now = new Date();
+
+  return Math.floor((now.getTime() - playedAt.getTime()) / 1000);
+}
+
+export function getPlayTime(seconds: number): string {
+  let secs = computeElapsedTime(seconds);
+
+  if (secs >= -60 && secs < 0) {
+    secs *= -1;
+    return `Played in ${secs} seconds`;
+  } else if (secs < -60) {
+    secs *= -1;
+    return `Played in ${secondsToMinutes(secs, 2)} minutes`;
+  } else if (secs > 0 && secs < 30) {
+    return `Ready to play`;
+  } else {
+    return `${secondsToMinutes(secs, 0, true, false)} minutes ago`;
+  }
+}
+
+export function updateTimestamps(): void {
+  const timestamps = getAllElementByQuery<HTMLParagraphElement>(
+    "[data-played-timestamp]"
+  );
+  if (!timestamps) {
+    console.error("No timestamp elements found!");
+    return;
+  }
+
+  timestamps.forEach((element) => {
+    if (!element.dataset.playedTimestamp) {
+      console.error("No played timestamp found for element:", element);
+      return;
+    }
+    element.textContent = getPlayTime(Number(element.dataset.playedTimestamp));
+  });
+}
+
+export function getRandomPosition(): {
+  x: number;
+  y: number;
+} {
+  const maxX = window.innerWidth * 0.75; // Adjust for element width
+  const maxY = window.innerHeight * 0.75; // Adjust for element height
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  return { x: randomX, y: randomY };
+}
+
+export function setBackgroundRandomPos(): void {
+  const elements = getAllElementByQuery<HTMLDivElement>("[data-palette-id]");
+  if (!elements) {
+    console.error("No bg palette elements found!");
+    return;
+  }
+
+  elements.forEach((element) => {
+    const { x, y } = getRandomPosition();
+    element.style.setProperty("left", `${x}px`);
+    element.style.setProperty("top", `${y}px`);
+  });
+}
+
+export function updateBackgroundColors(
+  [dR, dG, dB]: number[],
+  palette: number[][]
+) {
+  const backgroundDominant =
+    getElementById<HTMLDivElement>("backgroundDominant");
+  const backgroundPalettes =
+    getAllElementByQuery<HTMLDivElement>("[data-palette-id]");
+  if (!backgroundDominant) {
+    console.error("No background dominant element found!");
+    return;
+  } else {
+    backgroundDominant.style.setProperty(
+      "--bg-dominant-color",
+      `rgb(${dR}, ${dG}, ${dB})`
+    );
+  }
+
+  if (!backgroundPalettes) {
+    console.error("No background palette elements found!");
+    return;
+  } else {
+    backgroundPalettes.forEach((element, index) => {
+      if (!element.dataset.paletteId) {
+        console.error("No palette id found for element:", element);
+        return;
+      }
+
+      if (!palette[index]) {
+        console.error(
+          "No palette found for index:",
+          index,
+          "of element:",
+          element
+        );
+        return;
+      }
+
+      const pR = palette[index][0];
+      const pG = palette[index][1];
+      const pB = palette[index][2];
+      element.style.setProperty(
+        "--bg-palette-color",
+        `rgb(${pR}, ${pG}, ${pB})`
+      );
+    });
+  }
+}
+
+export function extractCoverColorsAndSetBackgroundColors() {
+  const playerCover = getElementById<HTMLImageElement>("playerCover");
+  if (!playerCover) {
+    console.error("No player cover found!");
+    return;
+  }
+
+  const CT = new ColorThief();
+  if (playerCover.complete) {
+    const dominant = CT.getColor(playerCover);
+    const palette = CT.getPalette(playerCover, 9);
+    updateBackgroundColors([...dominant], [...palette]);
+  } else {
+    playerCover.addEventListener("load", function () {
+      const dominant = CT.getColor(playerCover);
+      const palette = CT.getPalette(playerCover, 9);
+      updateBackgroundColors([...dominant], [...palette]);
+    });
+  }
+}
